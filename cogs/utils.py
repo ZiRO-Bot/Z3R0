aab18ee939e5ca86201e483f2454a9c1a6d6355f
ziro <palembani@gmail.com>
2020-08-08 06:30:35 +0700

    + Send deleted messages to purgatory channel

--- cogs/utils.py
+++ cogs/utils.py
@@ -5,6 +5,20 @@ import asyncio
 class Utils(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        purgatory_ch = self.bot.get_channel(741431840958578811)
+        embed = discord.Embed(title = "Deleted Message",
+                colour = discord.Colour.red())
+        embed.add_field(name="User", value=f"{message.author.mention}")
+        embed.add_field(name="Channel", value=f"{message.channel.mention}")
+        if not message.content:
+            msg = "None"
+        else:
+            msg = message.content
+        embed.add_field(name="Message", value=f"{msg}")
+        await purgatory_ch.send(embed=embed)
 
     @commands.command()
     async def ping(self, ctx):
