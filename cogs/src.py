a38c546a49424ff9d0d56e8782c61f003be25494
ziro <palembani@gmail.com>
2020-08-08 09:35:06 +0700

    + Added wrs command (grab wrs for specific category)

--- cogs/src.py
+++ cogs/src.py
@@ -7,6 +7,49 @@ import json
 import asyncio
 import dateutil.parser
 
+async def worldrecord(self, ctx, category: str="Any"):
+    head = {"Accept": "application/json", "User-Agent": "ziBot/0.1"}
+    game = "mcbe"
+    cat = category
+    wrs = {
+            'cat': '',
+            'platform': '',
+            'runner': '',
+            'link': '',
+            'time': ''
+            }
+    platformsVar = json.loads(requests.get(
+            f"https://www.speedrun.com/api/v1/variables/38dj2ex8",
+            headers=head).text)
+    platforms = platformsVar['data']['values']['choices']
+    catName = json.loads(requests.get(
+        f"https://www.speedrun.com/api/v1/leaderboards/yd4ovvg1/category/{cat}?embed=category",
+        headers=head).text)['data']['category']['data']['name']
+    wrs['cat']=catName
+    embed = discord.Embed(
+            title=f"{wrs['cat']} MCBE World Records",
+            colour=discord.Colour.gold()
+            )
+    for platform in platforms:
+        wr = json.loads(requests.get(
+                "https://www.speedrun.com/api/v1/leaderboards/"+
+                f"{game}/category/{cat}?top=1&var-38dj2ex8={platform}",
+                headers=head).text)
+        wrData = json.loads(requests.get(
+                "https://www.speedrun.com/api/v1/runs/"+
+                f"{wr['data']['runs'][0]['run']['id']}"+
+                "?embed=players,level,platform",
+                headers=head).text)['data']
+        wrs['platform']=platforms[platform]
+        if wrData['players']["data"][0]['rel'] == 'guest':
+            wrs['runner']=wrData['players']['data'][0]['names']
+        else:
+            wrs['runner']=wrData['players']["data"][0]["names"]["international"]
+        wrs['link']=wrData['weblink']
+        wrs['time']=timedelta(seconds=wrData['times']['realtime_t'])
+        embed.add_field(name=f"{wrs['platform']}",value=f"{wrs['runner']} ({wrs['time']})",inline=False)
+    await ctx.send(embed=embed)
+
 async def pendingrun(self, ctx):
     mcbe_runs = 0
     mcbeil_runs = 0
@@ -70,10 +113,11 @@ async def pendingrun(self, ctx):
             await self.bot.get_channel(741199490391736340).send(embed=embed)
     runs = runs2
     gameID = gameID2
+    total = mcbe_runs + mcbece_runs + mcbeil_runs
     embed_stats = discord.Embed(
                 title='Pending Run Stats',
                 description=
-                f"Full Game Runs: {mcbe_runs}\nIndividual Level Runs: {mcbeil_runs}\nCategory Extension Runs: {mcbece_runs}",
+                f"Full Game Runs: {mcbe_runs}\nIndividual Level Runs: {mcbeil_runs}\nCategory Extension Runs: {mcbece_runs}\n**Total: {total}**",
                 color=16711680 + i * 60)
     await self.bot.get_channel(741199490391736340).send(embed=embed_stats)
 
@@ -89,9 +133,11 @@ class Src(commands.Cog):
             await pendingrun(self, ctx)
     
     @commands.command()
-    async def wrs(self, ctx):
-        """Get world record runs from speedun.com"""
-        await ctx.send("Not available yet.")
+    async def wrs(self, ctx, category: str="any"):
+        """Get world record runs from speedun.com
+        `NOTE: type the category without '%' (e.g. >wrs Any)`"""
+        async with ctx.typing():
+            await worldrecord(self, ctx, category)
 
 def setup(bot):
     bot.add_cog(Src(bot))
