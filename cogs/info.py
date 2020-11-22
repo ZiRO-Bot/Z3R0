7b06d43fa15fec5c82613d4bfdfee18a98e00589
ziro <palembani@gmail.com>
2020-11-22 09:07:09 +0700

    + Added license command

--- cogs/info.py
+++ cogs/info.py
@@ -591,6 +591,24 @@ class Info(commands.Cog):
             text=f"Requested by {ctx.message.author.name}#{ctx.message.author.discriminator}"
         )
         await ctx.send(embed=e)
+
+    @commands.command()
+    async def license(self, ctx):
+        """Show bot's license."""
+        _license = "ziBot is a multi-purpose, customizable, open-source discord bot.\n" \
+                + "Copyright (C) 2020  Ahmad Ansori Palembani\n\n" \
+                + "This program is free software: you can redistribute it and/or modify" \
+                + "it under the terms of the GNU General Public License as published by" \
+                + "the Free Software Foundation, either version 3 of the License, or" \
+                + "(at your option) any later version.\n\n" \
+                + "This program is distributed in the hope that it will be useful," \
+                + "but WITHOUT ANY WARRANTY; without even the implied warranty of" \
+                + "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the" \
+                + "GNU General Public License for more details.\n\n" \
+                + "You should have received a copy of the GNU General Public License" \
+                + "along with this program.  If not, see <https://www.gnu.org/licenses/>."
+        e = discord.Embed(title="License", colour=discord.Colour(0xFFFFF0), description=f"**GNU GPL-3.0-or-Later**\n```{_license}```")
+        await ctx.send(embed=e)
     
 
 def setup(bot):
