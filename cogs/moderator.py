e536ebe7a535ea3ae3011ed65d4891eb515b8952
ziro <palembani@gmail.com>
2020-11-28 10:22:29 +0700

    + Move bot master stuff from moderator to developer.py

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -21,9 +21,6 @@ from discord.errors import Forbidden, NotFound
 from discord.ext import commands
 from typing import Optional
 
-SHELL = os.getenv("SHELL") or "/bin/bash"
-WINDOWS = sys.platform == "win32"
-
 ch_types = {
     "general": ["general", "Regular text channel"],
     "voice": ["voice", "Voice channel"],
@@ -99,91 +96,6 @@ class Moderation(commands.Cog):
             return ctx.author.id in ctx.bot.master
 
         return commands.check(predicate)
-
-    @commands.command(aliases=["quit"], hidden=True)
-    @checks.is_botmaster()
-    async def force_close(self, ctx):
-        """Shutdown the bot."""
-        await ctx.send("Shutting down...")
-        await ctx.bot.logout()
-
-    @commands.command(usage="(extension)", hidden=True)
-    @checks.is_botmaster()
-    async def unload(self, ctx, ext):
-        """Unload an extension."""
-        await ctx.send(f"Unloading {ext}...")
-        try:
-            self.bot.unload_extension(f"cogs.{ext}")
-            await ctx.send(f"{ext} has been unloaded.")
-        except commands.ExtensionNotFound:
-            await ctx.send(f"{ext} doesn't exist!")
-        except commands.ExtensionNotLoaded:
-            await ctx.send(f"{ext} is not loaded!")
-        except commands.ExtensionFailed:
-            await ctx.send(f"{ext} failed to unload! Check the log for details.")
-            self.bot.logger.exception(f"Failed to reload extension {ext}:")
-
-    @commands.command(usage="[extension]", hidden=True)
-    @checks.is_botmaster()
-    async def reload(self, ctx, ext: str = None):
-        """Reload an extension."""
-        if not ext:
-            reload_start = time.time()
-            exts = get_cogs()
-            reloaded = []
-            error = 0
-            for ext in exts:
-                try:
-                    self.bot.reload_extension(f"{ext}")
-                    reloaded.append(f"<:check_mark:747274119426605116>| {ext}")
-                except commands.ExtensionNotFound:
-                    reloaded.append(f"<:check_mark:747271588474388522>| {ext}")
-                    error += 1
-                except commands.ExtensionNotLoaded:
-                    reloaded.append(f"<:cross_mark:747274119275479042>| {ext}")
-                    error += 1
-                except commands.ExtensionFailed:
-                    self.bot.logger.exception(f"Failed to reload extension {ext}:")
-                    reloaded.append(f"<:cross_mark:747274119275479042>| {ext}")
-                    error += 1
-            reloaded = "\n".join(reloaded)
-            embed = discord.Embed(
-                title="Reloading all cogs...",
-                description=f"{reloaded}",
-                colour=discord.Colour(0x2F3136),
-            )
-            embed.set_footer(
-                text=f"{len(exts)} cogs has been reloaded"
-                + f", with {error} errors \n"
-                + f"in {realtime(time.time() - reload_start)}"
-            )
-            await ctx.send(embed=embed)
-            return
-        await ctx.send(f"Reloading {ext}...")
-        try:
-            self.bot.reload_extension(f"cogs.{ext}")
-            await ctx.send(f"{ext} has been reloaded.")
-        except commands.ExtensionNotFound:
-            await ctx.send(f"{ext} doesn't exist!")
-        except commands.ExtensionNotLoaded:
-            await ctx.send(f"{ext} is not loaded!")
-        except commands.ExtensionFailed:
-            await ctx.send(f"{ext} failed to reload! Check the log for details.")
-            self.bot.logger.exception(f"Failed to reload extension {ext}:")
-
-    @commands.command(usage="(extension)", hidden=True)
-    @checks.is_botmaster()
-    async def load(self, ctx, ext):
-        """Load an extension."""
-        await ctx.send(f"Loading {ext}...")
-        try:
-            self.bot.load_extension(f"cogs.{ext}")
-            await ctx.send(f"{ext} has been loaded.")
-        except commands.ExtensionNotFound:
-            await ctx.send(f"{ext} doesn't exist!")
-        except commands.ExtensionFailed:
-            await ctx.send(f"{ext} failed to load! Check the log for details.")
-            self.bot.logger.exception(f"Failed to reload extension {ext}:")
 
     @commands.command(aliases=["cc"], usage="(amount of chat)", hidden=True)
     @checks.is_mod()
@@ -422,28 +334,6 @@ class Moderation(commands.Cog):
                     f"{member.mention} has been unbanned by {ctx.author.mention}!"
                 )
 
-    @commands.command(hidden=True)
-    @checks.is_botmaster()
-    async def pull(self, ctx):
-        """Update the bot from github."""
-        g = git.cmd.Git(os.getcwd())
-        embed = discord.Embed(
-            title="Git",
-            colour=discord.Colour.lighter_gray(),
-            timestamp=datetime.datetime.now(),
-        )
-        try:
-            embed.add_field(name="Pulling...", value=f"```bash\n{g.pull()}```")
-        except git.exc.GitCommandError as e:
-            embed.add_field(name="Pulling...", value=f"```bash\n{e}```")
-        await ctx.send(embed=embed)
-
-    @commands.command()
-    @checks.is_botmaster()
-    async def leave(self, ctx):
-        """Leave the server."""
-        await ctx.message.guild.leave()
-
     @commands.group()
     @checks.is_mod()
     async def channel(self, ctx):
@@ -537,31 +427,6 @@ class Moderation(commands.Cog):
             match = re.match(r"Channel \"[0-9]*\" not found.", str(error))
             if match:
                 await em_ctx_send_error(ctx, "You can only set a text channel's type!")
-
-    @commands.command(aliases=["sh"], usage="(shell command)", hidden=True)
-    @checks.is_botmaster()
-    async def shell(self, ctx, *command: str):
-        """Execute shell command from discord. **Use with caution**"""
-        if WINDOWS:
-            sequence = shlex.split(" ".join([*command]))
-        else:
-            sequence = [SHELL, "-c", " ".join([*command])]
-
-        proc = subprocess.Popen(sequence, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-
-        def clean_bytes(line):
-            """
-            Cleans a byte sequence of shell directives and decodes it.
-            """
-            lines = line
-            line = []
-            for i in lines:
-                line.append(i.decode("utf-8"))
-            line = "".join(line)
-            text = line.replace("\r", "").strip("\n")
-            return re.sub(r"\x1b[^m]*m", "", text).replace("``", "`\u200b`").strip("\n")
-
-        await ctx.send(f"```{clean_bytes(proc.stdout.readlines())}```")
 
     @commands.group()
     @checks.is_mod()
