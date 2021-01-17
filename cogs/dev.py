956d0b5b24a5ea793319f79921dcc136c75bcb4c
ziro <palembani@gmail.com>
2021-01-17 17:38:19 +0700

    + Idk shell command thingy

--- cogs/dev.py
+++ cogs/dev.py
@@ -3,6 +3,8 @@ import discord
 import os
 import re
 import sys
+import subprocess
+import time
 
 from asyncio.subprocess import PIPE, STDOUT
 from discord.ext import commands, menus
@@ -13,42 +15,117 @@ SHELL = os.getenv("SHELL") or "/bin/bash"
 WINDOWS = sys.platform == "win32"
 
 
-class ShellResult:
-    def __init__(self, status, stdout, stderr):
-        self.status = status
-        self._stdout = stdout or ""
-        self._stderr = stderr or ""
-        if stdout is not None:
-            self.stdout = stdout.decode("utf-8")
+def background_reader(stream, loop: asyncio.AbstractEventLoop, callback):
+    """
+    Reads a stream and forwards each line to an async callback.
+    """
+
+    for line in iter(stream.readline, b''):
+        loop.call_soon_threadsafe(loop.create_task, callback(line))
+
+
+class ShellHandler:
+    def __init__(self, code: str, timeout: int = 90, loop: asyncio.AbstractEventLoop = None):
+        if WINDOWS:
+            raise TypeError("Windows is not supported!")
         else:
-            self.stdout = None
-        if stderr is not None:
-            self.stderr = stderr.decode("utf-8")
-        else:
-            self.stderr = None
+            sequence = [SHELL, "-c", code]
 
-    def __repr__(self):
-        return f"<Result status={self.status} stdout={len(self._stdout)} stderr={len(self._stderr)}>"
+        self.process = subprocess.Popen(sequence, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.close_code = None
+
+        self.loop = loop or asyncio.get_event_loop()
+        self.timeout = timeout
+
+        self.stdout_task = self.make_reader_task(self.process.stdout, self.stdout_handler)
+        self.stderr_task = self.make_reader_task(self.process.stderr, self.stderr_handler)
+
+        self.queue = asyncio.Queue(maxsize=250)
+
+    @property
+    def closed(self):
+        """
+        Are both tasks done, indicating there is no more to read?
+        """
+
+        return self.stdout_task.done() and self.stderr_task.done()
+
+    async def executor_wrapper(self, *args, **kwargs):
+        """
+        Call wrapper for stream reader.
+        """
+
+        return await self.loop.run_in_executor(None, *args, **kwargs)
+
+    def make_reader_task(self, stream, callback):
+        """
+        Create a reader executor task for a stream.
+        """
+
+        return self.loop.create_task(self.executor_wrapper(background_reader, stream, self.loop, callback))
+
+    @staticmethod
+    def clean_bytes(line):
+        """
+        Cleans a byte sequence of shell directives and decodes it.
+        """
+
+        text = line.decode('utf-8').replace('\r', '').strip('\n')
+        return re.sub(r'\x1b[^m]*m', '', text).replace("``", "`\u200b`").strip('\n')
+
+    async def stdout_handler(self, line):
+        """
+        Handler for this class for stdout.
+        """
+
+        await self.queue.put(self.clean_bytes(line))
+
+    async def stderr_handler(self, line):
+        """
+        Handler for this class for stderr.
+        """
+
+        await self.queue.put(self.clean_bytes(b'[stderr] ' + line))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.process.kill()
+        self.process.terminate()
+        self.close_code = self.process.wait(timeout=0.5)
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        start_time = time.perf_counter()
+
+        while not self.closed or not self.queue.empty():
+            try:
+                return await asyncio.wait_for(self.queue.get(), timeout=1)
+            except asyncio.TimeoutError as exception:
+                if time.perf_counter() - start_time >= self.timeout:
+                    raise exception
+
+        raise StopAsyncIteration()
 
 
-class TextWrapPageSource(menus.ListPageSource):
-    def __init__(self, prefix, lang, raw_text, max_size: int = 1024):
-        size_limit = len(prefix) * 2 + len(lang) + max_size
-        text = [raw_text]
-        n = 0
-        while len(text[n]) > size_limit:
-            text.append(text[n][size_limit:])
-            text[n] = text[n][:size_limit]
-            n += 1
-        super().__init__(entries=text, per_page=1)
-        self.lang = lang
-        self.prefix = prefix + lang + "\n"
-        self.suffix = prefix
+class PaginatorSource(commands.Paginator, menus.PageSource):
+    def is_paginating(self):
+        return self.get_max_pages() > 1
 
+    def get_max_pages(self):
+        return len(self.pages)
+
+    async def get_page(self, page_number: int):
+        return self.pages[page_number]
+
+class ShellPageSource(PaginatorSource):
     async def format_page(self, menu, text):
         e = discord.Embed(
             title="Shell",
-            description=self.prefix + text + self.suffix,
+            description=text,
             colour=discord.Colour(0xFFFFF0),
         )
         return e
@@ -68,39 +145,14 @@ class Developer(commands.Cog):
         if "sudo" in command:
             return
 
-        if WINDOWS:
-            return await ctx.send("Windows is not supported!")
-        else:
-            sequence = [SHELL, "-c", " ".join([*command])]
+        with ShellHandler(command) as reader:
+            p = ShellPageSource(("`" * 3) + "sh", "`" * 3, 1024)
+            p.add_line(f"$ {command}\n")
+            async for line in reader:
+                p.add_line(line)
 
-        async def run(shell_command):
-            p = await asyncio.create_subprocess_shell(
-                shell_command, stdin=PIPE, stdout=PIPE, stderr=STDOUT
-            )
-            stdout, stderr = await p.communicate()
-            code = p.returncode
-            return ShellResult(code, stdout, stderr)
-
-        proc = await run(command)
-
-        def clean_bytes(line):
-            """
-            Cleans a byte sequence of shell directives and decodes it.
-            """
-            # lines = line
-            # line = []
-            # for i in lines:
-            #     line.append(i.decode("utf-8"))
-            # line = "".join(line)
-            text = line.replace("\r", "").strip("\n")
-            return re.sub(r"\x1b[^m]*m", "", text).replace("``", "`\u200b`").strip("\n")
-
-        content = (
-            clean_bytes(proc.stdout + (" " + proc.stderr if proc.stderr else ""))
-            or f"{SHELL}: command not found: {' '.join(command)}"
-        )
-        menus = ZiMenu(TextWrapPageSource("```", "sh", content))
-        return await menus.start(ctx)
+            menus = ZiMenu(p)
+            await menus.start(ctx)
 
     @commands.command()
     async def pull(self, ctx):
