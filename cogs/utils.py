a13aa6ac08b0d771aeb44eef80fe21c226658a31
ziro <palembani@gmail.com>
2021-01-26 07:14:44 +0700

    + Disable translate

--- cogs/utils.py
+++ cogs/utils.py
@@ -318,34 +318,36 @@ class Utils(commands.Cog):
         e.add_field(name="Bot Latency", value=f"{round(msg_ping)}ms")
         await msg.edit(embed=e)
 
-    @commands.command(
-        aliases=["trans"], brief="Translate a text.", usage="(language) (text)"
-    )
-    async def translate(self, ctx, lang, *txt):
-        """Translate a text.\n\
-           **Example**
-           ``>translate ja Hello World``"""
-        if not txt:
-            await ctx.send("You need to specify the text you want to translate!")
-        abbv = {"jp": "ja"}
-        if lang in abbv:
-            lang = abbv[lang]
-        translation = await translator.translate(" ".join(txt), dest=lang)
-        # remove spaces from <@![ID]>
-        translated = str(translation.text).replace("<@! ", "<@!")
-        translated = str(translated).replace("<@ ", "<@")
-        translated = str(translated).replace("<# ", "<#")
-        embed = discord.Embed(timestamp=ctx.message.created_at)
-        embed.set_author(
-            name="Google Translate", icon_url="https://translate.google.com/favicon.ico"
-        )
-        embed.add_field(
-            name=f"Source [{translation.src}]", value=translation.origin, inline=False
-        )
-        embed.add_field(
-            name=f"Translated [{translation.dest}]", value=translated, inline=False
-        )
-        await ctx.send(embed=embed)
+    # Disabled due to module not working properly
+    # TODO: find new API to work with
+    # @commands.command(
+    #     aliases=["trans"], brief="Translate a text.", usage="(language) (text)"
+    # )
+    # async def translate(self, ctx, lang, *txt):
+    #     """Translate a text.\n\
+    #        **Example**
+    #        ``>translate ja Hello World``"""
+    #     if not txt:
+    #         await ctx.send("You need to specify the text you want to translate!")
+    #     abbv = {"jp": "ja"}
+    #     if lang in abbv:
+    #         lang = abbv[lang]
+    #     translation = await translator.translate(" ".join(txt), dest=lang)
+    #     # remove spaces from <@![ID]>
+    #     translated = str(translation.text).replace("<@! ", "<@!")
+    #     translated = str(translated).replace("<@ ", "<@")
+    #     translated = str(translated).replace("<# ", "<#")
+    #     embed = discord.Embed(timestamp=ctx.message.created_at)
+    #     embed.set_author(
+    #         name="Google Translate", icon_url="https://translate.google.com/favicon.ico"
+    #     )
+    #     embed.add_field(
+    #         name=f"Source [{translation.src}]", value=translation.origin, inline=False
+    #     )
+    #     embed.add_field(
+    #         name=f"Translated [{translation.dest}]", value=translated, inline=False
+    #     )
+    #     await ctx.send(embed=embed)
 
     @commands.command(usage="(project name)")
     async def pypi(self, ctx, project: str):
