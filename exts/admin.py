1f931a88d821b25ffee02b903db2bab619db90d0
ziro <palembani@gmail.com>
2021-03-11 14:33:52 +0700

    + Added group "command", no function yet.

--- exts/admin.py
+++ exts/admin.py
@@ -1,7 +1,9 @@
 import aiosqlite
 import discord
 
+
 from discord.ext import commands
+    
 
 class Admin(commands.Cog):
     def __init__(self, bot):
@@ -39,6 +41,50 @@ class Admin(commands.Cog):
             """
         )
         await self.db.commit()
-    
+
+    # TODO: Adds custom check with usage limit (
+    #     0: Only mods,
+    #     1: Partial (Can only add/edit/remove their own command),
+    #     2: Full (Can do anything to any existing command in the guild)
+    # )
+    @commands.group()
+    async def command(self, ctx):
+        """Manage commands"""
+        pass
+
+    @command.command(aliases=["+", "create"])
+    async def add(self, ctx, name, *content):
+        """Add new command"""
+        pass
+
+    @command.command(aliases=["&"])
+    async def edit(self, ctx, name, *content):
+        """Edit existing command"""
+        pass
+
+    @command.command(aliases=["-", "rm"])
+    async def remove(self, ctx, name):
+        """Remove a command"""
+        pass
+
+    @command.command()
+    async def disable(self, ctx, name):
+        """Disable a command"""
+        # This will work for both built-in and user-made commands
+        pass
+
+    @command.command()
+    async def enable(self, ctx, name):
+        """Enable a command"""
+        # This will work for both built-in and user-made commands
+        pass
+
+    @command.command(aliases=["?"])
+    async def info(self, ctx, name):
+        """Show information about a command"""
+        # Executes {prefix}help {name} if its built-in command
+        pass
+
+
 def setup(bot):
     bot.add_cog(Admin(bot))
