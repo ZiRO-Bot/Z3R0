5880d97b29dc88396b872ba6c4734e6e9591138b
ziro <palembani@gmail.com>
2021-05-21 16:26:07 +0700

    + Add sql query for `youtube` cog/ext

--- exts/utils/dbQuery.py
+++ exts/utils/dbQuery.py
@@ -1,7 +1,8 @@
-# Just bunch of SQL query
+"""Just bunch of SQL query."""
+
 createCommandsTable = """
     CREATE TABLE IF NOT EXISTS commands (
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
         description TEXT,
         content TEXT,
@@ -29,3 +30,26 @@ createGuildsTable = """
 """
 
 insertToGuilds = "INSERT OR IGNORE INTO guilds (id) VALUES (:id)"
+
+# --- Alpha feature, not pushed to git yet (Still debating if i should add it or not).
+createYTChannelTable = """
+    CREATE TABLE IF NOT EXISTS yt_channels (
+        channelId TEXT PRIMARY KEY,
+        expiredAt INTEGER
+    )
+"""
+
+createYTSentVideoTable = """
+    CREATE TABLE IF NOT EXISTS yt_sent (
+        videoId TEXT PRIMARY KEY
+    )
+"""
+
+createYTGuildSubsTable = """
+    CREATE TABLE IF NOT EXISTS yt_subs (
+        guildId INTEGER NOT NULL,
+        channelId TEXT,
+        FOREIGN KEY ("channelId") REFERENCES yt_channels ("channelId") ON DELETE CASCADE
+    )
+"""
+# ---
