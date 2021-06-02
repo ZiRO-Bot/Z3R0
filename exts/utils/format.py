88ba83f1af91ffe1ed7e489aabe88d659a562b85
ziro <palembani@gmail.com>
2021-06-02 14:26:29 +0700

    + Strip spaces at the beginning and the end of cmdname

--- exts/utils/format.py
+++ exts/utils/format.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 
 def formatName(name: str):
-    return name.lower().replace(" ", "-")
+    return name.strip().lower().replace(" ", "-")
 
 class CMDName(commands.Converter):
     async def convert(self, ctx, argument: str):
