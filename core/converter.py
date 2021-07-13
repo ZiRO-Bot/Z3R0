156d6e14da26d38396baa207ab02123356d9f5f0
ziro <palembani@gmail.com>
2021-07-13 11:01:43 +0700

    + Added BannedMember converter

--- core/converter.py
+++ core/converter.py
@@ -44,3 +44,21 @@ class TimeAndArgument(commands.Converter):
 
         # TODO: Try to parse "specific time" (8:00am or 16 Jun 2021)
         return self
+
+
+# https://github.com/Rapptz/RoboDanny/blob/rewrite/cogs/mod.py#L109-L123
+class BannedMember(commands.Converter):
+    async def convert(self, ctx, argument):
+        if argument.isdigit():
+            member_id = int(argument, base=10)
+            try:
+                return await ctx.guild.fetch_ban(discord.Object(id=member_id))
+            except discord.NotFound:
+                raise commands.BadArgument('This member has not been banned before.') from None
+
+        ban_list = await ctx.guild.bans()
+        entity = discord.utils.find(lambda u: str(u.user) == argument, ban_list)
+
+        if entity is None:
+            raise commands.BadArgument('This member has not been banned before.')
+        return entity
