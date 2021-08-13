7e1b6da946e1e7d766637730762bc136b025f563
ziro <palembani@gmail.com>
2021-08-13 10:46:37 +0700

    + Added `on_check_failed`

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from discord import Interaction
 from discord.ui import View
 
@@ -9,6 +11,13 @@ class ZView(View):
         super().__init__(**kwargs)
         self.owner = owner
 
+    async def on_check_failed(self, interaction: Interaction, *args, **kwargs) -> Any:
+        """Just incase i need to replace failed check response"""
+        if owner := self.owner:
+            await interaction.response.send_message(
+                f"This interaction belongs to {owner.mention}", ephemeral=True
+            )
+
     async def interaction_check(self, interaction: Interaction) -> bool:
         owner = self.owner
         if not owner:
@@ -16,8 +25,6 @@ class ZView(View):
 
         # Prevent other user other than interaction owner using this interaction
         if interaction.user.id != owner.id:
-            await interaction.response.send_message(
-                f"This interaction belongs to {owner.mention}", ephemeral=True
-            )
+            await self.on_check_failed(interaction)
             return False
         return True
