48521228a3413bbc0fbc4ea3044f6f901ba443d7
ziro <palembani@gmail.com>
2022-03-06 15:04:36 +0700

    + [ 3 files changed ] - Simplify piglin loot table

--- src/main/utils/piglin.py
+++ src/main/utils/piglin.py
@@ -3,61 +3,104 @@ import random
 
 # Loot table based on MCJE 1.16.1
 LOOT_TABLE = [
-    (5, "enchanted-book"),
-    (8, "iron-boots"),
-    (10, "iron-nugget"),
-    (10, "splash-potion-fire-res"),
-    (10, "potion-fire-res"),
-    (20, "quartz"),
-    (20, "glowstone-dust"),
-    (20, "magma-cream"),
-    (20, "ender-pearl"),
-    (20, "string"),
-    (40, "fire-charge"),
-    (40, "gravel"),
-    (40, "leather"),
-    (40, "nether-brick"),
-    (40, "obsidian"),
-    (40, "cry-obsidian"),
-    (40, "soul-sand"),
+    {
+        "id": "enchanted-book",
+        "name": "Enchanted Book - Soul Speed",
+        "weight": 5,
+    },
+    {
+        "id": "iron-boots",
+        "name": "Enchanted Iron Boots - Soul Speed",
+        "weight": 8,
+    },
+    {
+        "id": "iron-nugget",
+        "name": "Iron Nuggets",
+        "weight": 10,
+        "quantity": (9, 36),
+    },
+    {
+        "id": "splash-potion-fire-res",
+        "name": "Splash Potion - Fire Resistance",
+        "weight": 10,
+    },
+    {
+        "id": "potion-fire-res",
+        "name": "Potion - Fire Resistance",
+        "weight": 10,
+    },
+    {
+        "id": "quartz",
+        "name": "Nether Quartz",
+        "weight": 20,
+        "quantity": (8, 16),
+    },
+    {
+        "id": "glowstone-dust",
+        "name": "Glowstone Dust",
+        "weight": 20,
+        "quantity": (5, 12),
+    },
+    {
+        "id": "magma-cream",
+        "name": "Magma Cream",
+        "weight": 20,
+        "quantity": (2, 6),
+    },
+    {
+        "id": "ender-pearl",
+        "name": "Ender Pearls",
+        "weight": 20,
+        "quantity": (4, 8),
+    },
+    {
+        "id": "string",
+        "name": "String",
+        "weight": 20,
+        "quantity": (8, 24),
+    },
+    {
+        "id": "fire-charge",
+        "name": "Fire Charge",
+        "weight": 40,
+        "quantity": (1, 5),
+    },
+    {
+        "id": "gravel",
+        "name": "Gravel",
+        "weight": 40,
+        "quantity": (8, 16),
+    },
+    {
+        "id": "leather",
+        "name": "Leather",
+        "weight": 40,
+        "quantity": (4, 10),
+    },
+    {
+        "id": "nether-brick",
+        "name": "Nether Bricks",
+        "weight": 40,
+        "quantity": (4, 16),
+    },
+    {
+        "id": "obsidian",
+        "name": "Obsidian",
+        "weight": 40,
+    },
+    {
+        "id": "cry-obsidian",
+        "name": "Crying Obsidian",
+        "weight": 40,
+        "quantity": (1, 3),
+    },
+    {
+        "id": "soul-sand",
+        "name": "Soul Sand",
+        "weight": 40,
+        "quantity": (4, 16),
+    },
 ]
-
-# Item's name
-NAME = {
-    "enchanted-book": "Enchanted Book - Soul Speed",
-    "iron-boots": "Enchanted Iron Boots - Soul Speed",
-    "iron-nugget": "Iron Nuggets",
-    "splash-potion-fire-res": "Splash Potion - Fire Resistance",
-    "potion-fire-res": "Potion - Fire Resistance",
-    "quartz": "Nether Quartz",
-    "glowstone-dust": "Glowstone Dust",
-    "magma-cream": "Magma Cream",
-    "ender-pearl": "Ender Pearls",
-    "string": "String",
-    "fire-charge": "Fire Charge",
-    "gravel": "Gravel",
-    "leather": "Leather",
-    "nether-brick": "Nether Bricks",
-    "obsidian": "Obsidian",
-    "cry-obsidian": "Crying Obsidian",
-    "soul-sand": "Soul Sand",
-}
-
-# Item's quantity range
-QUANTITY = {
-    "iron-nugget": (9, 36),
-    "quartz": (8, 16),
-    "glowstone-dust": (5, 12),
-    "magma-cream": (2, 6),
-    "ender-pearl": (4, 8),
-    "string": (8, 24),
-    "fire-charge": (1, 5),
-    "gravel": (8, 16),
-    "leather": (4, 10),
-    "nether-brick": (4, 16),
-    "cry-obsidian": (1, 3),
-    "soul-sand": (4, 16),
-}
 
 
 class Piglin:
@@ -69,12 +112,10 @@ class Piglin:
     """
 
     def __init__(self, gold: int = 64):
-        items = []
         weights = []
         for loot in LOOT_TABLE:
-            weights.append(loot[0])
-            items.append(loot[1])
-        self.items = [BarterItem(random.choices(items, weights=weights)[0]) for i in range(gold)]
+            weights.append(loot["weight"])
+        self.items = [BarterItem(random.choices(LOOT_TABLE, weights=weights)[0]) for i in range(gold)]
 
     def __str__(self):
         return ", ".join(["{}: {}".format(str(item), item.quantity) for item in self.items])
@@ -84,10 +125,10 @@ class BarterItem:
 
     __slots__ = ("id", "name", "quantity")
 
-    def __init__(self, _id):
-        self.id = _id
-        self.name = NAME.get(_id)
-        q = QUANTITY.get(_id, 1)
+    def __init__(self, item):
+        self.id = item["id"]
+        self.name = item["name"]
+        q = item.get("quantity", 1)
         self.quantity = q if not isinstance(q, tuple) else random.randrange(q[0], q[1])
 
     def __str__(self):
