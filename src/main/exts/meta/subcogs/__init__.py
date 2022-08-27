099083cc2cccd87f8b4e6939592ee5f40500a8ca
ziro <palembani@gmail.com>
2022-08-27 11:21:29 +0700

    + [ 2 files changed ] - Renamed custom command subcog filename

--- src/main/exts/meta/subcogs/__init__.py
+++ src/main/exts/meta/subcogs/__init__.py
@@ -3,4 +3,4 @@ This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
-from .meta_custom_commands import MetaCustomCommands
+from .meta__custom_commands import MetaCustomCommands
