3f3cd7529961c3f574fc055c592b9239550795fc
ziro <palembani@gmail.com>
2023-02-16 10:59:45 +0700

    test: Add more tests

--- src/test/test_ext_meta.py
+++ src/test/test_ext_meta.py
@@ -6,6 +6,8 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from __future__ import annotations
 
+import asyncio
+
 import discord.ext.test as dpytest
 import pytest
 
@@ -14,34 +16,26 @@ from main.core.embed import ZEmbed
 
 
 @pytest.mark.asyncio
-async def testPing(bot: ziBot):
-    msg = await dpytest.message(">ping")
-    ctx = await bot.get_context(msg)
-    e = ZEmbed.default(ctx, title="Pong!")
-    e.add_field(
-        name="<a:discordLoading:857138980192911381> | Websocket",
-        value="∞",
-    )
-    e.add_field(
-        name="<a:typing:785053882664878100> | Typing",
-        value="0ms",
-        inline=False,
-    )
-
-    be = dpytest.get_embed()
-
-    # dpytest don't test fields for some reason
-    assert all([dpytest.utils.embed_eq(e, be), all([f == be.fields[i] for i, f in enumerate(e.fields)])])
+async def testNothingIsEmpty(bot: ziBot):
+    """Test the rest of the commands in Meta that doesn't need DB"""
+    await dpytest.message(">source")
+    assert not dpytest.verify().message().nothing()
+    await dpytest.message(">about")
+    assert not dpytest.verify().message().nothing()
+    await dpytest.message(">stats")
+    assert not dpytest.verify().message().nothing()
 
 
 @pytest.mark.asyncio
 async def testPrefixAdd(bot: ziBot):
+    """Test prefix addition"""
     await dpytest.message(">prefix + !")
     assert str(dpytest.get_embed(peek=True).title).endswith("added")
 
 
 @pytest.mark.asyncio
 async def testPrefixUnique(bot: ziBot):
+    """Test failed prefix addition (already exists)"""
     await dpytest.message(">prefix + !")
     await dpytest.message(">prefix + !")
     assert str(dpytest.get_embed(peek=True).description).endswith("exists")
@@ -49,6 +43,7 @@ async def testPrefixUnique(bot: ziBot):
 
 @pytest.mark.asyncio
 async def testPrefixFull(bot: ziBot):
+    """Test failed prefix addition (full)"""
     await dpytest.message(">prefix + 1")
     await dpytest.message(">prefix + 2")
     await dpytest.message(">prefix + 3")
@@ -70,6 +65,7 @@ async def testPrefixFull(bot: ziBot):
 
 @pytest.mark.asyncio
 async def testPrefixRemove(bot: ziBot):
+    """Test prefix removal"""
     await dpytest.message(">prefix + !")
     await dpytest.message(">prefix - !")
     assert str(dpytest.get_embed(peek=True).title).endswith("removed")
@@ -77,5 +73,40 @@ async def testPrefixRemove(bot: ziBot):
 
 @pytest.mark.asyncio
 async def testPrefixNotExists(bot: ziBot):
+    """Test failed prefix removal (prefix not exists)"""
     await dpytest.message(">prefix - !")
     assert str(dpytest.get_embed(peek=True).description).endswith("exists")
+
+
+@pytest.mark.asyncio
+async def testPrefixList(bot: ziBot):
+    """Test whether custom prefix fetched properly or not"""
+    await dpytest.message(">prefix list")
+    assert str(dpytest.get_embed(peek=True).description).endswith("No custom prefix.")
+    await dpytest.message(">prefix + !")
+    await asyncio.sleep(5)  # Cooldown
+    await dpytest.message(">prefix list")
+    assert str(dpytest.get_embed(peek=True).description).endswith("`!`")
+
+
+@pytest.mark.asyncio
+async def testPing(bot: ziBot):
+    """Test executing "ping" with custom prefix"""
+    await dpytest.message(">prefix + !")
+    msg = await dpytest.message("!ping")
+    ctx = await bot.get_context(msg)
+    e = ZEmbed.default(ctx, title="Pong!")
+    e.add_field(
+        name="<a:discordLoading:857138980192911381> | Websocket",
+        value="∞",
+    )
+    e.add_field(
+        name="<a:typing:785053882664878100> | Typing",
+        value="0ms",
+        inline=False,
+    )
+
+    be = dpytest.get_embed(peek=True)
+
+    # dpytest don't test fields for some reason
+    assert all([dpytest.utils.embed_eq(e, be), all([f == be.fields[i] for i, f in enumerate(e.fields)])])
