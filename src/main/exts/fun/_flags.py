4051dbc69e47876805a0a0247bd20bd31171a7de
ziro <palembani@gmail.com>
2023-02-17 11:01:21 +0700

    fix: Properly fix findseed flag not falling back to 'visual'

--- src/main/exts/fun/_flags.py
+++ src/main/exts/fun/_flags.py
@@ -7,7 +7,7 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from typing import Literal
 
 from discord.ext import commands
-from discord.ext.commands.errors import BadLiteralArgument
+from discord.ext.commands.errors import BadFlagArgument, BadLiteralArgument
 
 
 FINDSEED_MODES = Literal["visual", "classic", "pipega", "halloween"]
@@ -22,5 +22,5 @@ class FindseedFlags(commands.FlagConverter, case_insensitive=True):
             mode = f"mode: {mode}"
         try:
             return await super().convert(context, mode)
-        except BadLiteralArgument:
+        except (BadLiteralArgument, BadFlagArgument):
             return "visual"
