f01f15608c9b47b13a8400fd72ea3722fec34d89
ziro <palembani@gmail.com>
2023-02-28 17:24:16 +0700

    test: Prefix list should be sent when bot is mentioned

---
+++ src/test/test_core_bot.py
@@ -0,0 +1,20 @@
+"""
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+
+from __future__ import annotations
+
+import discord.ext.test as dpytest
+import pytest
+from discord.ext.commands.errors import BadFlagArgument
+
+from main.core.bot import ziBot
+
+
+@pytest.mark.asyncio
+async def testBotMentioned(bot: ziBot):
+    """Test prefix list being sent when bot is mentioned"""
+    await dpytest.message(bot.user.mention)  # type: ignore
+    assert not dpytest.verify().message().nothing()
