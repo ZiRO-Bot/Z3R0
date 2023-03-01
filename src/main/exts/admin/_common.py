c8888304bf8efb69a1d135edf19a6261d5ba401e
ziro <palembani@gmail.com>
2023-03-01 14:18:48 +0700

    refactor: Moved views out of `exts.admin`

---
+++ src/main/exts/admin/_common.py
@@ -0,0 +1,84 @@
+"""
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+
+from __future__ import annotations
+
+import discord
+from discord.utils import MISSING
+
+from ...core import flags
+from ...core.context import Context
+from ...core.embed import ZEmbed
+from ...core.guild import GuildWrapper
+from ...utils.format import separateStringFlags
+from ._views import OpenGreetingModal
+
+
+async def handleGreetingConfig(
+    ctx: Context,
+    type: str,
+    *,
+    arguments=MISSING,
+    message: str = None,
+    raw: bool = False,
+    disable: bool = False,
+    channel: discord.TextChannel = None,
+):
+    """Handle welcome and farewell configuration."""
+    guild: GuildWrapper | None = ctx.guild
+    if not guild:
+        raise RuntimeError
+
+    changeMsg = False
+
+    if arguments is None:
+        # TODO - Revisit once more input introduced to modals
+        # TODO - Timeout + disable the button
+        defMsg = await ctx.bot.getGuildConfig(guild.id, f"{type}Msg")
+
+        await ctx.try_reply(
+            "This feature currently not yet available on Mobile!\n"
+            "If you're on Mobile, please do `{}{} "
+            "[message] [options]` instead".format(ctx.clean_prefix, type),
+            view=OpenGreetingModal(ctx, type, defMsg, owner=ctx.author),
+        )
+        return
+    elif arguments is not MISSING:
+        message, args = separateStringFlags(arguments)
+
+        parsed = await flags.GreetingFlags.convert(ctx, args)
+
+        # Parsed value from flags
+        disable = parsed.disable
+        raw = parsed.raw
+        channel = parsed.channel
+        message = " ".join([message.strip()] + parsed.messages).strip()
+
+    if not raw and not disable and message:
+        changeMsg = True
+
+    e = ZEmbed.success(
+        title=("Welcome" if type == "welcome" else "Farewell") + " config has been updated",
+    )
+
+    if disable is True:
+        await ctx.bot.setGuildConfig(guild.id, f"{type}Ch", None, "GuildChannels")
+        e.add_field(name="Status", value="`Disabled`")
+        return await ctx.try_reply(embed=e)
+
+    if raw is True:
+        message = await ctx.bot.getGuildConfig(guild.id, f"{type}Msg")
+        return await ctx.try_reply(discord.utils.escape_markdown(str(message)))
+
+    if changeMsg:
+        await ctx.bot.setGuildConfig(guild.id, f"{type}Msg", message)
+        e.add_field(name="Message", value=message, inline=False)
+
+    if channel is not None:
+        await ctx.bot.setGuildConfig(guild.id, f"{type}Ch", channel.id, "GuildChannels")
+        e.add_field(name="Channel", value=channel.mention)
+
+    return await ctx.try_reply(embed=e)
---
+++ src/main/exts/admin/_views.py
@@ -0,0 +1,75 @@
+"""
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+
+from __future__ import annotations
+
+import discord
+
+from ...core.context import Context
+from ...core.views import ZView
+from ._common import handleGreetingConfig
+
+
+class Greeting(discord.ui.Modal, title="Greeting"):
+    message = discord.ui.TextInput(
+        label="Message", placeholder="Welcome, {user(mention)}! {react: 👋}", style=discord.TextStyle.paragraph
+    )
+
+    # TODO - hopefully discord will add channel input soon into modal
+    #        for now i'll comment this
+    # channel = discord.ui.TextInput(
+    #     label="Channel",
+    #     placeholder="336642139381301249",
+    # )
+
+    def __init__(
+        self,
+        context: Context,
+        type: str,
+        *,
+        defaultMessage: str | None = None,
+    ) -> None:
+        super().__init__(title=type.title())
+        self.context = context
+        self.type = type
+        self.message.default = defaultMessage
+
+    async def callback(self):
+        await handleGreetingConfig(self.context, self.type, message=self.message)  # type: ignore
+
+    async def on_submit(self, inter: discord.Interaction):
+        await self.callback()
+        return await inter.response.defer()
+
+
+class OpenGreetingModal(ZView):
+    def __init__(self, context: Context, type, defaultMessage, message: discord.Message | None = None, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.context = context
+        self.message = message
+        self.type = type
+        self.defaultMessage = defaultMessage
+
+    async def on_timeout(self) -> None:
+        if not self.message:
+            return
+
+        for item in self.children:
+            if isinstance(item, discord.Button):
+                item.disabled = True
+
+        await self.message.edit(view=self)
+
+    @discord.ui.button(label='Configure')
+    async def configureGreeting(self, interaction: discord.Interaction, button: discord.ui.Button):
+        modal = Greeting(self.context, self.type, defaultMessage=self.defaultMessage)
+        await interaction.response.send_modal(modal)
+
+        if not self.message:
+            return
+
+        button.disabled = True
+        await self.message.edit(view=self)
