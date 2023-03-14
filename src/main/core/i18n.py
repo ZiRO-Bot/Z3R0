0713022980bbafad9f34f98cae5f1a729851221b
ziro <palembani@gmail.com>
2023-03-14 16:47:57 +0700

    refactor: Use fluent.runtime internal API

--- src/main/core/i18n.py
+++ src/main/core/i18n.py
@@ -8,114 +8,56 @@ from __future__ import annotations
 
 from contextlib import suppress
 from pathlib import Path
-from typing import TYPE_CHECKING, Any
+from typing import Any, cast
 
-import fluent.syntax.ast as FTL
+import discord
+from fluent.runtime import FluentBundle
 from fluent.syntax import FluentParser
+from fluent.syntax.ast import Resource
 
 
-if TYPE_CHECKING:
-    from fluent.syntax.ast import Resource
-
-
-class FluentType:
-    def __init__(self):
-        raise NotImplementedError
-
-
-class FluentIdentifier(FluentType):
-    def __init__(self, name: str, **kwargs):
-        self.name: str = name
-
-    def __str__(self):
-        return self.name
-
-    def __repr__(self):
-        return f"Identifier={self.name}"
-
-
-class FluentMessage(FluentType):
-    def __init__(self, id: FluentIdentifier, value, attributes: list, **kwargs):
-        self.id = id
-        self.value = value
-        self.attributes = attributes
-
-
-class FluentPattern(FluentType):
-    def __init__(self, elements: list, **kwargs):
-        self.elements: list[FluentElement] = elements
-
-
-class FluentElement(FluentType):
-    def __init__(self, value, **kwargs):
-        self.value = value
-
-    def __str__(self):
-        return str(self.value)
-
-
-class FluentTextElement(FluentElement):
-    pass
-
-
-class FluentResolver:
-    @staticmethod
-    def resolveMessage(_, **kwargs):
-        return FluentMessage(**kwargs)
-
-    @staticmethod
-    def resolvePattern(node, elements, **kwargs):
-        if len(elements) == 1:
-            return FluentResolver.resolveTextElement(node, value=elements[0].value)  # TODO
-        return FluentPattern(elements, **kwargs)
-
-    @staticmethod
-    def resolveTextElement(_, **kwargs):
-        return FluentTextElement(**kwargs)
-
-    @staticmethod
-    def resolveIdentifier(_, **kwargs):
-        return FluentIdentifier(**kwargs)
-
-
-class FluentCompiler:
-    def compile(self, node: Any):
-        if isinstance(node, FTL.BaseNode):
-            nodename = type(node).__name__
-            if not hasattr(FluentResolver, f"resolve{nodename}"):
-                return node
-
-            kwargs: dict[str, Any] = vars(node).copy()
-            newKwargs: dict[str, Any] = {}
-            for k, v in kwargs.items():
-                newKwargs[k] = self.compile(v)
-            attr = getattr(FluentResolver, f"resolve{nodename}", None)
-            if attr:
-                return attr(node, **newKwargs)
-        if isinstance(node, (tuple, list)):
-            return [self.compile(i) for i in node]
-        return node
-
-
-class FluentProvider:
-    def __init__(self):
+class Localization:
+    def __init__(self, defaultLocale: discord.Locale = discord.Locale.american_english):
+        self.defaultLocale: discord.Locale = defaultLocale
         self.root = Path("src/main/locale")
-        self.bundles: dict[str, Any] = {}
-        self.compiler = FluentCompiler()
+        self._defaultResource: Resource = self._getResource(defaultLocale)
+        self.bundles: dict[discord.Locale, Any] = {}
 
-    def get(self, msgId: str, locale: str) -> FluentMessage:
-        with Path(self.root, f"{locale}.ftl").open() as fp:
-            bundle = FluentParser().parse(fp.read())
-        if not self.bundles.get(locale):
-            self.bundles[locale] = {}
-            for item in bundle.body:
-                compiled = self.compiler.compile(item)
-                if isinstance(compiled, FluentMessage):
-                    self.bundles[locale][str(compiled.id)] = compiled
-        return self.bundles[locale][msgId]
+        self._bundle(defaultLocale)
 
-    def translate(self, msgId: str, locale: str, args: dict[str, Any] | None = None):
-        return self.get(msgId, locale)
+    def _bundle(self, locale: discord.Locale):
+        if locale in self.bundles:
+            return self.bundles[locale]
+
+        bundle = FluentBundle([str(locale)])
+        with suppress(FileNotFoundError):
+            bundle.add_resource(self._getResource(locale))
+        bundle.add_resource(self._defaultResource)
+        self.bundles[locale] = bundle
+        return bundle
+
+    def get(self, msgId: str, locale: discord.Locale, args: dict[str, Any] | None = None) -> str:
+        bundle = self._bundle(locale)
+        if not bundle.has_message(msgId):
+            return msgId
+
+        msg = bundle.get_message(msgId)
+        if not msg.value:
+            return msgId
+
+        val, _ = bundle.format_pattern(msg.value, args)
+        return cast(str, val)
+
+    def format(self, msgId: str, locale: discord.Locale | None = None) -> str:
+        return self.get(msgId, locale or self.defaultLocale)
+
+    def _getResource(self, locale: discord.Locale) -> Resource:
+        with Path(self.root, f"{locale.value}.ftl").open() as fp:
+            resource = FluentParser().parse(fp.read())
+        return resource
 
 
-print(FluentProvider().translate("test", "en_US").value)
+test = Localization()
+print(test.format("test"))
+print(test.format("test", discord.Locale.japanese))
+print(test.format("test", discord.Locale.indonesian))
