e9587bd5c6d17b85c4a81a97e4891c3e45f4ff75
ziro <palembani@gmail.com>
2023-03-20 19:36:38 +0700

    feat: WIP i18n friendly embed builder

--- src/main/core/embed.py
+++ src/main/core/embed.py
@@ -6,9 +6,10 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Union
+from typing import TYPE_CHECKING, Collection
 
 import discord
+from discord.app_commands import locale_str
 
 from ..utils.other import utcnow
 from .enums import Emojis
@@ -18,11 +19,27 @@ if TYPE_CHECKING:
     from ..core.context import Context
 
 
+class Field:
+    def __init__(self, name: locale_str | str, value: locale_str | str, inline: bool = False):
+        self.name: locale_str | str = name
+        self.value: locale_str | str = value
+        self.inline: bool = inline
+
+
 class ZEmbed(discord.Embed):
-    def __init__(self, color=0x3DB4FF, fields=(), field_inline=False, **kwargs):
+    def __init__(self, color=0x3DB4FF, fields: Collection[Field] = list(), fieldInline=False, **kwargs):
         super().__init__(color=color, **kwargs)
-        for n, v in fields:
-            self.add_field(name=n, value=v, inline=field_inline)
+        for field in fields:
+            self.add_field(name=field.name, value=field.value, inline=field.inline or fieldInline)
+
+    # TODO: Not yet "usable", just an idea for now
+    @classmethod
+    async def asyncBuild(cls, context: Context, fields: Collection[Field] = list(), **kwargs):
+        instance = cls(**kwargs)
+        for field in fields:
+            name = await context.maybeTranslate(field.name)
+            value = await context.maybeTranslate(field.value)
+            instance.add_field(name=name, value=value, inline=field.inline or kwargs.get("fieldInline", False))
 
     @classmethod
     def minimal(cls, timestamp=None, **kwargs):
@@ -30,17 +47,32 @@ class ZEmbed(discord.Embed):
         return instance
 
     @classmethod
-    def default(cls, context: Union[discord.Interaction, Context], timestamp=None, **kwargs):
+    def default(cls, context: discord.Interaction | Context, timestamp=None, **kwargs):
+        """Shortcut to build embeds"""
         try:
-            author = context.author
+            author = context.author  # type: ignore
         except AttributeError:
-            author = context.user
+            author = context.user  # type: ignore
 
         instance = cls.minimal(timestamp=timestamp or utcnow(), **kwargs)
         instance.set_footer(
             text="Requested by {}".format(author),
             icon_url=author.display_avatar.url,
         )
+        return instance
+
+    # TODO: Not yet "usable", just an idea for now
+    @classmethod
+    async def asyncDefault(
+        cls, context: Context | discord.Interaction, *, title: locale_str | str, timestamp=None, **kwargs
+    ) -> ZEmbed:
+        """|coro|
+
+        Shortcut to build embed faster that support i18n
+        """
+        if isinstance(title, locale_str):
+            title = await context.translate(title) or title.message
+        instance = cls.default(context, title=title, timestamp=timestamp, **kwargs)
         return instance
 
     @classmethod
