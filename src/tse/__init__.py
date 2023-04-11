7b77f5d40d3e97899b90d96dade4700165c35fb6
ziro <palembani@gmail.com>
2023-04-11 07:25:07 +0700

    chore: Move LICENSE and COPYING.md file to root folder GitHub can't detect them in .github/ folder

--- src/tse/__init__.py
+++ src/tse/__init__.py
@@ -1,10 +1,11 @@
 from .adapter import *
 from .block import *
 from .exceptions import *
-from .interface import Adapter, Block
+from .interface import Adapter as Adapter
+from .interface import Block as Block
 from .interpreter import *
 from .utils import *
-from .verb import Verb
+from .verb import Verb as Verb
 
 
 __version__ = '2.5.13a'
